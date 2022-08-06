520b5c097f129c0d2b3d74623150fb2e832e147b
Shruti <sdnk1993@gmail.com>
2022-08-06 17:12:43 +0530

    created UC1 branch

--- welcome.sh
+++ welcome.sh
@@ -1,3 +1,14 @@
 #!bin/bash
 
 echo "welcome to wage computation"
+
+present=1;
+
+isPresent=$((RANDOM%2));
+
+if [ $isPresent -eq $present ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
